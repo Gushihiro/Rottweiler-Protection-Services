fa51a38d757f1e8da0fcf188574d080a472aff85
Gushihiro <robinsonhiroto@gmail.com>
2021-09-16 11:52:01 -0700

    removed archaic code

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Header from '../components/Header'
-import ContactForm from '../components/ContactForm'
 import Footer from '../components/Footer'
 
 export default function Contact() {
