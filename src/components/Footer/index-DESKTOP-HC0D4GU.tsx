e13097dadb98d560b40f2ff72bd096734ac5b032
Gushihiro <robinsonhiroto@gmail.com>
2021-11-11 13:33:36 -0800

    changed footer stuff

--- src/components/Footer/index-DESKTOP-HC0D4GU.tsx
+++ src/components/Footer/index-DESKTOP-HC0D4GU.tsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function footer() {
   return (
     <div className='footer'>
-      <h4>© Rottweiler Protection Services LLC  - <a href={'https://gushihiro.github.io'}>Gushihiro WebDesign ©</a></h4>
+      <h4>© Rottweiler Protection Services LLC  - <a href={'https://gushihiro.me'}>Gushihiro WebDesign ©</a></h4>
     </div>
   )
 }
